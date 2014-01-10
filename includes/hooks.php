48a4da63f4ca3e65fd1dff43ff548e800b908806
John Morris <johntylermorris@jtmorris.net>
2014-01-09 20:26:23 -0800

    Update default shortcodes

--- includes/hooks.php
+++ includes/hooks.php
@@ -30,25 +30,25 @@ if( !class_exists( 'ABD_Hooks' ) ) {
 			$shortcodeAdditions[] = array( 
 				'name' => 'Default - 300px x 250px',
 				'noadblock' => '<div style="text-align: center; padding: 10px;border-radius: 15px; background-color: #585858; color: #FFFFFF; width: 300px; height: 250px"><p><b>YOUR AD HERE!</b></p><p>Go to the Ad Block Detector plugin\'s settings page to insert your ad code.</p></div>',
-				'adblock' => '<a href=""><img src="' . ABD_ROOT_URL . 'images/abd_default-300250.png" alt="You are using an ad blocker! Please consider disabling it to help us out." /></a>'
+				'adblock' => '<a target="_blank" href="http://adblockingdetector.jtmorris.net/"><img src="http://adblockingdetector.jtmorris.net/wp-content/uploads/2014/01/abd_default-300250.png" alt="You are using an ad blocker! Please consider disabling it to help us out." /></a>'
 			);
 			//	Default - 336px x 280px			
 			$shortcodeAdditions[] = array( 
 				'name' => 'Default - 336px x 280px',
 				'noadblock' => '<div style="text-align: center; padding: 10px;border-radius: 15px; background-color: #585858; color: #FFFFFF; width: 336px; height: 280px"><p><b>YOUR AD HERE!</b></p><p>Go to the Ad Block Detector plugin\'s settings page to insert your ad code.</p></div>',
-				'adblock' => '<a href=""><img src="' . ABD_ROOT_URL . 'images/abd_default-336280.png" alt="You are using an ad blocker! Please consider disabling it to help us out." /></a>'
+				'adblock' => '<a target="_blank" href="http://adblockingdetector.jtmorris.net/"><img src="http://adblockingdetector.jtmorris.net/wp-content/uploads/2014/01/abd_default-336280.png" alt="You are using an ad blocker! Please consider disabling it to help us out." /></a>'
 			);
 			//	Default - 728px x 90px			
 			$shortcodeAdditions[] = array( 
 				'name' => 'Default - 728px x 90px',
 				'noadblock' => '<div style="text-align: center; padding: 10px;border-radius: 15px; background-color: #585858; color: #FFFFFF; width: 728px; height: 90px"><p><b>YOUR AD HERE!</b></p><p>Go to the Ad Block Detector plugin\'s settings page to insert your ad code.</p></div>',
-				'adblock' => '<a href=""><img src="' . ABD_ROOT_URL . 'images/abd_default-72890.png" alt="You are using an ad blocker! Please consider disabling it to help us out." /></a>'
+				'adblock' => '<a target="_blank" href="http://adblockingdetector.jtmorris.net/"><img src="http://adblockingdetector.jtmorris.net/wp-content/uploads/2014/01/abd_default-72890.png" alt="You are using an ad blocker! Please consider disabling it to help us out." /></a>'
 			);
 			//	Default - 160px x 600px			
 			$shortcodeAdditions[] = array( 
 				'name' => 'Default - 160px x 600px',
 				'noadblock' => '<div style="text-align: center; padding: 10px;border-radius: 15px; background-color: #585858; color: #FFFFFF; width: 160px; height: 600px"><p><b>YOUR AD HERE!</b></p><p>Go to the Ad Block Detector plugin\'s settings page to insert your ad code.</p></div>',
-				'adblock' => '<a href=""><img src="' . ABD_ROOT_URL . 'images/abd_default-160600.png" alt="You are using an ad blocker! Please consider disabling it to help us out." /></a>'
+				'adblock' => '<a target="_blank" href="http://adblockingdetector.jtmorris.net/"><img src="http://adblockingdetector.jtmorris.net/wp-content/uploads/2014/01/abd_default-160600.png" alt="You are using an ad blocker! Please consider disabling it to help us out." /></a>'
 			);
 			
 			foreach ($tableCreate as $sql) {
